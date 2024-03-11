c6b763fc10c13b4b60c7b4b91b1546c34b78bddb
HONGCHEOL <yhc9632@gmail.com>
2024-03-11 15:58:33 +0900

    20240311

--- ch02/ch02_5/src/pages/OnChange.tsx
+++ ch02/ch02_5/src/pages/OnChange.tsx
@@ -1,3 +1,35 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>;
+import type { ChangeEvent } from "react";
+
+/* preventDefault 메서드는 어떤 사용자 액션에 따라 이벤트가 발생
+했을 때 이 이벤트와 관련된 웹 브라우저의 기본 구현 내용을 실행하지 않게 한다 */
+/* <input> 요소가 제공하는 속성들은 React.InputHTMLAt
+tributes<HTMLInputElement> 형태로 얻을 수 있음 */
+/*
+  value=""대신 defaultValue=""를 사용하여 Warning을 줄였다.
+ */
+
+export default function OnChange() {
+  const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log("onChangeValue", e.target.value);
+  };
+  const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    console.log("onChangeChecked", e.target.checked);
+  };
+  const onChangeFiles = (e: ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    console.log("onChangeFiles", e.target.files);
+  };
+  // prettier-ignore
+  return (
+ <div>
+ <p>OnChange</p>
+ <input type="text" onChange={onChangeValue}
+ placeholder="type some text" defaultValue="Hello"/>
+ <input type="checkbox" onChange={onChangeChecked} defaultChecked/>
+ <input type="file" onChange={onChangeFiles} multiple accept="images/*" />
+ </div>
+ )
 }
--- ch02/ch02_5/src/pages/StopPropagation.tsx
+++ ch02/ch02_5/src/pages/StopPropagation.tsx
@@ -1,3 +1,22 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>;
+import type { SyntheticEvent } from "react";
+// 가까운부모에서 먼 부모쪽으로 이벤트가 버블링되며 전달되는것을 멈춤 event capturing
+export default function StopPropagation() {
+  const onDivClick = (e: SyntheticEvent) =>
+    console.log("click event bubbles on <div>");
+  const onButtonClick = (e: SyntheticEvent) => {
+    console.log("mouse click occurs on <button> and call stopPropagation");
+    /* <button>이
+      stopPropagation을 호출하여 이벤트를 캡처했으므로
+      이제 <button>의 부모 요소인 <div> 쪽에는 이벤트가 전달되지 않습니다. 
+    */
+    e.stopPropagation();
+  };
+  return (
+    <div onClick={onDivClick}>
+      <p>StopPropagation</p>
+      <button onClick={onButtonClick}>
+        Click Me and stop event propagation
+      </button>
+    </div>
+  );
 }
