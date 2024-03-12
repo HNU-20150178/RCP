c7b5d48991ac872f349243efdffeb7fccfcea4da
HONGCHEOL <yhc9632@gmail.com>
2024-03-12 13:31:09 +0900

    20240312_02

--- ch03/ch03_1/src/components/Icon.tsx
+++ ch03/ch03_1/src/components/Icon.tsx
@@ -1,15 +1,13 @@
-import { render } from "@testing-library/react";
-import { CSSProperties, Component, FC, type FunctionComponent } from "react";
-
-export type IconProps = {
+import type { FC, DetailedHTMLProps, HTMLAttributes } from "react";
+type ReactSpanProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLSpanElement>,
+  HTMLSpanElement
+>;
+export type IconProps = ReactSpanProps & {
   name: string;
-  style?: CSSProperties;
 };
-
-export const Icon: FC<IconProps> = ({ name, ...props }) => {
-  return (
-    <span {...props} className="material-icons">
-      {name}
-    </span>
-  );
-};
+// prettier-ignore
+export const Icon: FC<IconProps> = ({name, className: _className, ...props}) => {
+ const className = ['material-icons', _className].join(' ')
+ return <span {...props} className={className}>{name}</span>
+}
