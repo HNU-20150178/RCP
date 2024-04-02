e5b032307bb5e8752d150640aac110b01b12956c
HONGCHEOL <yhc9632@gmail.com>
2024-04-02 16:04:01 +0900

    20240402

--- ch04/ch04_3/src/pages/ArrayState.tsx
+++ ch04/ch04_3/src/pages/ArrayState.tsx
@@ -1,8 +1,40 @@
-export default function CopyMe() {
+import { useCallback, useState, useMemo } from 'react'
+import { Title, Div } from '../components'
+import { Icon } from '../theme/daisyui'
+import * as D from '../data'
+export default function ArrayState() {
+  const [images, setImages] = useState<string[]>([]) // 배열을 상태로 만들기
+  const addImage = useCallback(
+    //  () => setImages((images) => [...images, D.randomImage(200, 100, 50)]), // 전개 연산자가 앞쪽에 있으므로 새로 생성되는 이미지 정보가 배열 맨 뒤에 위치한다.
+    () => setImages((images) => [D.randomImage(200, 100, 50), ...images]), // 전개 연산자가 뒤쪽에 있으므로 새로 생성되는 이미지 정보가 배열 맨 앞에 위치한다.
+    []
+  )
+  const clearImages = useCallback(() => setImages((notUsed) => []), []) // images를 빈 대괄호 []로 설정하여 초기화
+  const children = useMemo(
+    () =>
+      images.map((image, index) => (
+        <Div
+          key={index}
+          src={image}
+          className="w-1/5 m-2"
+          height="5rem"
+          minHeight="5rem"
+        />
+      )),
+    [images]
+  )
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ArrayState</Title>
+      <div className="flex justify-center mt-4 ">
+        <div data-tip="add image" className="tooltip">
+          <Icon name="add" onClick={addImage} className="mr-12 btn-primary" />
+        </div>
+        <div data-tip="clear all" className="tooltip">
+          <Icon name="clear_all" onClick={clearImages} />
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-4">{children}</div>
     </section>
-  );
+  )
 }
--- ch04/ch04_3/src/pages/ObjectState.tsx
+++ ch04/ch04_3/src/pages/ObjectState.tsx
@@ -1,8 +1,52 @@
-export default function CopyMe() {
+import type { FormEvent, ChangeEvent } from 'react'
+import { useCallback, useState } from 'react'
+import { Title } from '../components'
+export default function BasicForm() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault() //  웹 페이지가 다시 렌더링되지 않도록하기 위함
+      const formData = new FormData() // FormData는 자바스크립트 엔진이 기본으로 제공하는 클래스, 사용자가 입력한 데이터들을 웹 서버에 전송할 목적으로 사용합니다.
+      formData.append('name', name)
+      formData.append('email', email)
+      alert(JSON.stringify(Object.fromEntries(formData), null, 2)) // JSON 포멧으로 변환
+    },
+    [name, email]
+  )
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setName((notUsed) => e.target.value)
+  }, [])
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail((notUsed) => e.target.value)
+  }, [])
+  // prettier-ignore
   return (
-    <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
-  );
+ <section className="mt-4">
+ <Title>BasicForm</Title>
+ <div className="flex justify-center mt-4">
+ <form onSubmit={onSubmit}>
+ <div className="form-control">
+ <label className="label" htmlFor="name">
+ <span className="label-text">Username</span>
+ </label>
+ <input value={name} onChange={onChangeName} id="name" type="text"
+ placeholder="enter your name" className="input input-primary" />
+ </div>
+ <div className="form-control">
+ <label className="label" htmlFor="email">
+ <span className="label-text">email</span>
+ </label>
+ <input value={email} onChange={onChangeEmail} id="email" type="email"
+ placeholder="enter your email" className="input input-primary"/>
+ </div>
+ <div className="flex justify-center mt-4">
+ <input type="submit" value="Submit"
+ className="w-1/2 btn btn-sm btn-primary"/>
+ <input defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm"/>
+ </div>
+ </form>
+ </div>
+ </section>
+ )
 }
